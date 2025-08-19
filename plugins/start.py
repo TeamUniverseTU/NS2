dac9e0d82b9dbc489cf4b49f8492b417dc32e23d
TeamUniversTU <138324953+TeamUniverseTU@users.noreply.github.com>
2025-08-20 00:06:36 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -27,7 +27,7 @@ async def start_command(client: Client, message: Message):
 
         string = await decode(base64_string)
         argument = string.split("-")
-        
+
         ids = []
         if len(argument) == 3:
             try:
@@ -45,7 +45,7 @@ async def start_command(client: Client, message: Message):
                 client.LOGGER(__name__, client.name).warning(f"Error decoding ID: {e}")
                 return
         temp_msg = await message.reply("Wait A Sec..")
-        
+
         try:
             messages = await get_messages(client, ids)
         except Exception as e:
@@ -64,30 +64,40 @@ async def start_command(client: Client, message: Message):
             caption = (client.messages.get('CAPTION', '').format(previouscaption=msg.caption.html if msg.caption else msg.document.file_name) if bool(client.messages.get('CAPTION', '')) and bool(msg.document)
                        else ("" if not msg.caption else msg.caption.html))
 
-
             reply_markup = msg.reply_markup if not client.disable_btn else None
 
             try:
-                copied_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, 
-                                            reply_markup=(reply_markup if not client.disable_btn else None), protect_content=client.protect)
+                copied_msg = await msg.copy(
+                    chat_id=message.from_user.id,
+                    caption=caption,
+                    reply_markup=(reply_markup if not client.disable_btn else None),
+                    protect_content=client.protect
+                )
                 yugen_msgs.append(copied_msg)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                copied_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, 
-                                            reply_markup=(reply_markup if not client.disable_btn else None), protect_content=client.protect)
+                copied_msg = await msg.copy(
+                    chat_id=message.from_user.id,
+                    caption=caption,
+                    reply_markup=(reply_markup if not client.disable_btn else None),
+                    protect_content=client.protect
+                )
                 yugen_msgs.append(copied_msg)
             except Exception as e:
                 client.LOGGER(__name__, client.name).warning(f"Failed to send message: {e}")
                 pass
+
         if messages:
             if client.auto_del > 0:
                 enter = text
-                k = await client.send_message(chat_id=message.from_user.id, 
-                                          text=f'<blockquote><b><i>This File is deleting automatically in {humanize.naturaldelta(client.auto_del)}. Forward in your Saved Messages..!</i></b></blockquote>')
+                k = await client.send_message(
+                    chat_id=message.from_user.id,
+                    text=f'<blockquote><b><i>This File is deleting automatically in {humanize.naturaldelta(client.auto_del)}. Forward in your Saved Messages..!</i></b></blockquote>'
+                )
                 asyncio.create_task(delete_files(yugen_msgs, client, k, enter))
                 return
     else:
-        buttons = [[InlineKeyboardButton("⚠️ ᴀʙᴏᴜᴛ ⚠️", callback_data = "about"), InlineKeyboardButton("✌️ ᴏᴡɴᴇʀ ✌️", user_id = client.owner)]]
+        buttons = []
         if user_id in client.admins:
             buttons.insert(0, [InlineKeyboardButton("⛩️ ꜱᴇᴛᴛɪɴɢꜱ ⛩️", callback_data="settings")])
         photo = client.messages.get("START_PHOTO", "")
@@ -101,10 +111,9 @@ async def start_command(client: Client, message: Message):
                     username=None if not message.from_user.username else '@' + message.from_user.username,
                     mention=message.from_user.mention,
                     id=message.from_user.id
-                        
                 ),
                 message_effect_id=MSG_EFFECT,
-                reply_markup=InlineKeyboardMarkup(buttons)
+                reply_markup=InlineKeyboardMarkup(buttons) if buttons else None
             )
         else:
             await client.send_message(
@@ -115,9 +124,8 @@ async def start_command(client: Client, message: Message):
                     username=None if not message.from_user.username else '@' + message.from_user.username,
                     mention=message.from_user.mention,
                     id=message.from_user.id
-                        
                 ),
                 message_effect_id=MSG_EFFECT,
-                reply_markup=InlineKeyboardMarkup(buttons)
+                reply_markup=InlineKeyboardMarkup(buttons) if buttons else None
             )
         return
